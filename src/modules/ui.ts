9da971bec4c94299ef459db7d9df4844d842abdb
yg-alt <4743933+yg-alt@users.noreply.github.com>
2025-07-07 09:51:28 +0000

    feat: add missing word

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -1,7 +1,7 @@
 import type { UserModule } from '~/types'
 import ElementPlus from 'element-plus'
 
-export const install: UserModule = ({ app }) => {
+export const install: UserModule = ({ isClient, app }) => {
   if (!isClient)
     return
   app.use(ElementPlus, { size: 'small', zIndex: 3000 })
