0eecea9177d5b95d2d981fa62512472a12a565b6
yg-alt <yg-alt@users.noreply.github.com>
2025-07-08 10:46:59 +0000

    deploy: 380ae83d7c97938a7a565e15f859f4e04f4fe148

--- assets/user-BvSEazN8.js
+++
@@ -1,1 +0,0 @@
-import{D as m,q as r,G as t}from"./app-JLRb_jNp.js";const l=m("user",()=>{const e=r(""),s=r(new Set),o=t(()=>Array.from(s.value)),u=t(()=>o.value.filter(a=>a!==e.value));function n(a){e.value&&s.value.add(e.value),e.value=a}return{setNewName:n,otherNames:u,savedName:e}});export{l as u};
